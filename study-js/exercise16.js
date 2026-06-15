00ab8159158b58056a977a8194822c6efb2148ca
Binnur KURT <deepcloudlabs@gmail.com>
2026-06-15 19:38:19 +0300

    initial commit

--- study-js/exercise16.js
+++ study-js/exercise16.js
@@ -1,3 +1,25 @@
+/*
+One JS thread
+One event loop
+Many concurrent socket operations managed by OS + libuv polling
+
+JavaScript main thread
+    |
+    | calls fetch(...)
+    v
+Node.js / undici / HTTP client layer
+    |
+    | opens socket / writes HTTP request
+    v
+Operating system networking stack
+    |
+    | waits for response asynchronously
+    v
+Event loop is notified when socket is readable
+    |
+    v
+JavaScript callback / Promise continuation runs on main thread
+ */
 import fetch from "node-fetch";
 function get_ticker(symbol){
     return new Promise((resolve)=>{
@@ -16,6 +38,7 @@ async function app(){
         get_ticker("ETHBTC"),
         get_ticker("GENIUSTRY")]
     );
+    tickers.sort((t1,t2)=>t1.symbol.localeCompare(t2.symbol));
     return tickers;
 }
 
