c7181e01f19ced7e24ed9fa08a8f118ba1a0b7c6
Binnur KURT <deepcloudlabs@gmail.com>
2026-06-16 15:00:25 +0300

    Create algotrading.ts

---
+++ study-ts/src/algotrading.ts
@@ -0,0 +1,43 @@
+import WebSocket from "ws";
+
+const wssUrlBinance: string = "wss://stream.binance.com:9443/ws/btcusdt@trade";
+const webSocket: WebSocket = new WebSocket(wssUrlBinance);
+
+type Trade = {
+    symbol: string;
+    quantity: number;
+    price: number;
+    volume: number;
+    timestamp: Date;
+    eventId: number;
+    sequence: number;
+}
+
+const trades : Trade[] = [];
+
+webSocket.on("open", function () {
+    console.log("Connected to binance.");
+    setInterval(function () {
+        let total_volume = trades.map(a_trade => a_trade.volume)
+            .map(v => Number(v))
+            .reduce((a, b) => a + b, 0);
+        let average_volume = total_volume / trades.length;
+        console.log(`Total volume: ${total_volume}, Average volume: ${average_volume}`);
+        trades.splice(0);
+
+    }, 30_000);
+})
+
+webSocket.on("message", function (raw: string) {
+    let payload = JSON.parse(raw);
+    let trade = {
+        symbol: payload.s,
+        quantity: Number(payload.q),
+        price: Number(payload.p),
+        volume: Number(payload.p) * Number(payload.q),
+        eventId: payload.E,
+        sequence: payload.t,
+        timestamp: new Date(payload.T)
+    };
+    trades.push(trade);
+})
