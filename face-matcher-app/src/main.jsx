b906215a976e0692394b54c4d7367831b9d587e3
Binnur KURT <deepcloudlabs@gmail.com>
2026-06-19 00:21:28 +0300

    initial commit

---
+++ face-matcher-app/src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CelebMatch from "./CelebMatch.jsx";
+
+ReactDOM.createRoot(document.getElementById('root')).render(<CelebMatch/>)
